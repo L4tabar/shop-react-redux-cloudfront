a9d9b0a0383cb1ad7e2542c3850f791f3bac9b28
Attila Jakubat <Attila_Jakubat@epam.com>
2025-09-17 15:17:06 +0200

    feat: changing the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    coverage: {
+      reporter: ["text", "lcov"],
+    },
   },
 });
